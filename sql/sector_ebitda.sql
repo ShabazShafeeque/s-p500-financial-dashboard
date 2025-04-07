6cf7f53b5b76733afcd79e613a6cf7245177e1c6
ShabazShafeeque <shabaz00017@gmail.com>
2025-04-08 02:02:08 +0530

    Update sector_ebitda.sql

--- sql/sector_ebitda.sql
+++ sql/sector_ebitda.sql
@@ -8,3 +8,15 @@ GROUP BY
 ORDER BY 
     AVG(EBITDA) DESC
 LIMIT 5;
+
+
+Output:
+-- +------------------------------+-----------------+
+-- | Sector                       | Sector_EBITDA   |
+-- +------------------------------+-----------------+
+-- | Information Technology       | 6161996057      |
+-- | Consumer Staples             | 5063167606      |
+-- | Energy                       | 4485798994      |
+-- | Health Care                  | 3642433186      |
+-- | Telecommunication Services   | 3365833333      |
+-- +------------------------------+-----------------+
