5a6b431c67239688799d68fbf1b626d7ee01b5e8
ShabazShafeeque <shabaz00017@gmail.com>
2025-04-08 02:02:35 +0530

    Update top_dividend_yield.sql

--- sql/top_dividend_yield.sql
+++ sql/top_dividend_yield.sql
@@ -6,3 +6,14 @@ FROM
 ORDER BY 
     `Dividend Yield` DESC
 LIMIT 5;
+
+
+ Output:
+-- +----------------------------+------------------+
+-- | Name                      | Dividend Yield    |
+-- +----------------------------+------------------+
+-- | CenturyLink Inc           | 12.661196         |
+-- | Kimco Realty              | 7.7134986         |
+-- | Iron Mountain Incorporated| 7.08258           |
+-- | Ford Motor                | 6.784366          |
+-- +----------------------------+------------------+
