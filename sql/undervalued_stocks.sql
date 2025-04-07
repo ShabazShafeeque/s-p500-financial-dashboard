cb070a6787ea146915271794a0ad9af2e1bb0a76
ShabazShafeeque <shabaz00017@gmail.com>
2025-04-08 01:57:46 +0530

    Update undervalued_stocks.sql

--- sql/undervalued_stocks.sql
+++ sql/undervalued_stocks.sql
@@ -12,3 +12,14 @@ ORDER BY
     `Price/Earnings` ASC, 
     `Price/Book` ASC
 LIMIT 5;
+
+Output:
+-- +---------------------+----------------+-------------+
+-- | Name                | Price/Earnings | Price/Book  |
+-- +---------------------+----------------+-------------+
+-- | Ford Motor          | 5.89           | 1.26        |
+-- | General Motors      | 6.58           | 1.33        |
+-- | Signet Jewelers     | 6.65           | 1.38        |
+-- | Navient             | 7.56           | 1.02        |
+-- | CenturyLink Inc     | 8.35           | 1.39        |
+-- +---------------------+----------------+-------------+
