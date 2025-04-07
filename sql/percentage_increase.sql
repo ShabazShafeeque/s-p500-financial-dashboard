09be8c9491c4a4be0a01a12737cba848d81ff2ad
ShabazShafeeque <shabaz00017@gmail.com>
2025-04-08 02:04:14 +0530

    Update percentage_increase.sql

--- sql/percentage_increase.sql
+++ sql/percentage_increase.sql
@@ -5,3 +5,15 @@ FROM
     finance
 ORDER BY 
     Percentage_Increase DESC;
+
+
+Output:
+-- +----------------------------+----------------------+
+-- | Name                       | Percentage_Increase  |
+-- +----------------------------+----------------------+
+-- | Align Technology           | 210.25               |
+-- | Envision Healthcare        | 207.11               |
+-- | Range Resources Corp.      | 168.43               |
+-- | Nvidia Corporation         | 161.92               |
+-- | Chesapeake Energy          | 135.36               |
+-- +----------------------------+----------------------+
